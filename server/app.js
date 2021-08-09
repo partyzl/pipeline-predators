f3e367861922a956eaca92501213d55581ee0733
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-08-09 16:43:55 +0100

    Posting experimentation and fixed typo in README

--- server/app.js
+++ server/app.js
@@ -1,12 +1,27 @@
 const express = require('express')
-const app = express();
+const cors = require('cors');
+const fs = require('fs');
 
-const entryRoute = require('./controller/entryController');
+const { entryData } = JSON.parse(fs.readFileSync('./data.json'));
+
+const app = express();
+app.use(cors());
+
+// const entryRoute = require('./controller/entryController');
 
 app.get('/', (req, res)=>{
     res.send('Shh you shouldn\'t be here')
 })
 
-app.use('/entries', entryRoute);
+// app.use('/entries', entryRoute);
+
+app.post('/entries', (req, res) => {
+    res.send({name: 'werty', message: 'asdf message'});
+})
+
+
+app.get('/entries', (req, res) => {
+    res.send(JSON.stringify(entryData));
+})
 
 module.exports = app;
