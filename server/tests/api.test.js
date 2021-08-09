35033773f794ef5f2fecb54dc8a6ef4c635f113b
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-08-09 10:17:28 +0100

    Initialised backend api tests

---
+++ server/tests/api.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const server = require('../app.js');
+
+describe('API Server', () => {
+    
+    const port = 5000;
+    let api;
+
+    beforeAll(() => {
+        api = server.listen(port, () => 
+            console.log(`Test server running on port ${port}`)
+        );
+    });
+
+    afterAll((done) => {
+        console.log('Stopping the test server.');
+        api.close(done);
+    });
+
+    test('Responds to get / with status 200', (done) => {
+        request(api).get('/').expect(200, done);
+    });
+});
