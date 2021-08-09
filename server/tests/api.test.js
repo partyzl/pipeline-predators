e554e82909b258cd5e848b13f8db2cae555c7462
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-08-09 17:27:40 +0100

    Test that non existing paths respond with 404

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -17,15 +17,19 @@ describe('API Server', () => {
         api.close(done);
     });
 
-    test('Responds to get / with status 200', (done) => {
+    test('Responds to get / with 200', (done) => {
         request(api).get('/').expect(200).expect('Shh you shouldn\'t be here', done);
     });
 
-    test('Responds to get /entries with status 200', (done) => {
+    test('Responds to get /entries with 200', (done) => {
         request(api).get('/entries').expect(200, done);
     });
 
-    test('responds to invalid method request with 405', (done) => {
+    test('Responds to non existing paths with 404', (done) => {
+        request(api).get('/asdf').expect(404, done);
+    });
+
+    test('Responds to invalid method request with 405', (done) => {
         request(api).post('/').expect(405, done);
     });
 });
