f244fd8d0a1f56dadd8ebd45844af872b42b8b58
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-08-10 14:57:19 +0100

    Added tests for journal entry section

--- client/tests/layout/new-post.test.js
+++ client/tests/layout/new-post.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+const exp = require('constants');
 const fs = require('fs');
 const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, '../../html/new-post.html'), 'utf8');
@@ -43,6 +44,29 @@ describe('new-post.html', () => {
                 expect(homeBtn.textContent).toContain('Home');
                 expect(createBtn.textContent).toContain('Create');
                 expect(postBtn.textContent).toContain('Post');
+            })
+        })
+
+        describe('Journal Entry Section', () => {
+            let label;
+            let textarea;
+            let small;
+
+            test('textarea exists and has max character length of 280', () => {
+                textarea = document.getElementById('entry');
+                expect(textarea).toBeTruthy();
+                expect(textarea.getAttribute('maxlength')).toBe('280');
+            })
+
+            test('textarea has a label', () => {
+                label = document.querySelector('[for="entry"]');
+                expect(label).toBeTruthy();
+            })
+
+            test('small tag', () => {
+                small = document.getElementById('entryHelp');
+                expect(small).toBeTruthy();
+                expect(small.textContent).toContain('characters remaining');
             })
         })
     })
