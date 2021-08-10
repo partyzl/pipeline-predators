b3a2ba444500c22b22d5db8b52a11f1cafacce3f
Parth Patel <parth6796@yahoo.com>
2021-08-10 17:13:01 +0100

    added to app.js page

--- server/app.js
+++ server/app.js
@@ -1,33 +1,29 @@
 const express = require('express')
 const cors = require('cors');
 const fs = require('fs');
+const bodyParser = require('body-parser')
 
 const { entryData } = JSON.parse(fs.readFileSync('./data.json'));
 
 const app = express();
 app.use(cors());
-
-// const entryRoute = require('./controller/entryController');
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
     res.send('Shh you shouldn\'t be here')
 })
 
-
 app.post('/', (req, res) => {
     res.send(405, 'Not allowd!');
 });
 
-// app.use('/entries', entryRoute);
-
-// app.use('/home', entryRoute);
 
 app.get('/home', (req,res) =>{
     res.send(JSON.stringify(entryData))
 })
 
 app.post('/create', (req, res) =>{
-    console.log(req.body);
+    res.send(201, req.body);
 })
 
 module.exports = app;
