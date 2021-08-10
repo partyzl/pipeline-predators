a61ad7fd8cea095b34676e16184242bcc4cf22dc
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-08-10 12:49:11 +0100

    Tests for the buttons existence and content

--- client/tests/layout/new-post.test.js
+++ client/tests/layout/new-post.test.js
@@ -11,13 +11,39 @@ describe('new-post.html', () => {
     
     beforeEach(() => {
         document.documentElement.innerHTML = html.toString();
-    });
+    })
 
     describe('head', () => {
         test('Title exists', () => {
             const title = document.querySelector('head > title');
             expect(title).toBeTruthy();
             expect(title.textContent).toBe('Create a New Post');
+        })
+    })
+
+    describe('body', () => {
+        describe('Buttons', () => {
+            let homeBtn;
+            let createBtn;
+            let postBtn;
+
+            beforeEach(() => {
+                homeBtn = document.getElementById('Home-Button');
+                createBtn = document.getElementById('Create-Entry-Button');
+                postBtn = document.getElementById('Post-Button');
+            })
+
+            test('The buttons exist', () => {
+                expect(homeBtn).toBeTruthy();
+                expect(createBtn).toBeTruthy();
+                expect(postBtn).toBeTruthy();
+            })
+
+            test('Buttons give info on what they do', () => {
+                expect(homeBtn.textContent).toContain('Home');
+                expect(createBtn.textContent).toContain('Create');
+                expect(postBtn.textContent).toContain('Post');
+            })
         })
     })
 })
