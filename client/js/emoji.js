e060392fb9464b77bbbe83e6424e178f3e984f2d
teganGowing <tgowing97@gmail.com>
2021-08-11 12:41:29 +0100

    'working on the emoji button toggle'

--- client/js/emoji.js
+++ client/js/emoji.js
@@ -6,35 +6,30 @@
 const { urlencoded } = require("express");
 
 
-const loveReact = document.getElementById('love');
-const sadReact = document.getElementById('sad');
-const shockReact = document.getElementById('shock');
 const reactButtons = document.getElementsByClassName('reactions')
 
 function emojiCount(reactionType){
-        switch (reactionType) {
-            case 'love':
-                fetch url(HERUKOURL)
-                response.json().then(data => {
-                    data.id[`${reactionType}`] =+;
-                    
-                    
-                
-                break;
-            case 'sad':
-                sadCount +=;
-                break;
-            case 'shock':
-                shockCount +=;
-                break;
-            default:
-                return false;
-        reactButtons.disable = true;
+    fetch url(HERUKOURL)
+    response.json().then(data => {
+        data.id[`${reactionType}`] =+;
+        }
+    .then(postID.label.`${reactionType}`.textContent = data.id[`${reactionType}`])
+    document.getElementById(`${reactionType}`)
+    emojiToggle(reactionType)
     
+}
+
+function emojiToggle(e){
+    if 
+/// emoji has clicked status and toggles other reactions to disabled
+}
+
+                    
+                    
+            
 
             
             }
     }
 }
 
-function
