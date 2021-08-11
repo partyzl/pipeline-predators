f1a41e542d7ac74cdc1f08f22e0b6684c18e826b
Parth Patel <parth6796@yahoo.com>
2021-08-11 12:45:33 +0100

    heroku work

--- client/js/script.js
+++ client/js/script.js
@@ -1,48 +1,49 @@
 //variables
-const herokuUrl = "https://pipeline-predators.herokuapp.com"
+const herokuUrl = "https://pipeline-predators.herokuapp.com";
 const newEntry = document.querySelector("#entry").value;
 const body = document.getElementById("main-body");
-const element = document.getElementById('entry')
+const element = document.getElementById("entry");
 //variables
 
 //display all entries on the page
-const getEntries=()=>{
-    fetch(`${herokuUrl}/home`)
-    //fetch(`https://localhost:3000/home`)
-    .then(function(response) {
-        if(response.status!== 200){
-            console.log('There was a problem accessing this site. Status Code: '+ response.status);
-        return;
-        }
-        //extract data from promise
-        response.json()
-        .then(function(entryData){
-           // console.log(entryData);
-            for(let i = 0; i<entryData.length; i++){
-                console.log(entryData[i].entry);
+const getEntries = () => {
+  //fetch(`${herokuUrl}/home`)
+  fetch(`https://localhost:3000/home`).then(function (response) {
+    if (response.status !== 200) {
+      console.log(
+        "There was a problem accessing this site. Status Code: " +
+          response.status
+      );
+      return;
+    }
+    //extract data from promise
+    response.json().then(function (entryData) {
+      // console.log(entryData);
+      for (let i = 0; i < entryData.length; i++) {
+        console.log(entryData[i].entry);
 
-                let row = document.createElement("section")
-                row.className = "";
+        let row = document.createElement("section");
+        row.className = "";
 
-                let newSection = document.createElement("section");
-                newSection.className = "alert alert-info";
-                let para = document.createElement('p');
-                para.textContent = `${entryData[i].entry}`
-                let paraComment = document.createElement('p');
-                paraComment.className = "comment";
-                paraComment.textContent = `${entryData[i].comments}`
+        let newSection = document.createElement("section");
+        newSection.className = "alert alert-info";
+        let para = document.createElement("p");
+        para.textContent = `${entryData[i].entry}`;
+        let paraComment = document.createElement("p");
+        paraComment.className = "comment";
+        paraComment.textContent = `${entryData[i].comments}`;
 
-                let entryArea = document.createElement("section");
-                entryArea.className = "col-md"
-                entryArea.appendChild(para);
-                entryArea.appendChild(paraComment);
-                
-                //still need to append reaction emotes here 
-                //maybe get svg's and add functions to up the counter on click
-            }
-        })
-    })
-}
+        let entryArea = document.createElement("section");
+        entryArea.className = "col-md";
+        entryArea.appendChild(para);
+        entryArea.appendChild(paraComment);
+
+        //still need to append reaction emotes here
+        //maybe get svg's and add functions to up the counter on click
+      }
+    });
+  });
+};
 
 // newEntry.addEventListener("submit", function(e){
 //     e.preventDefault();
@@ -66,25 +67,25 @@ const getEntries=()=>{
 //     });
 // }
 
-const data = {entry};
-element.addEventListener("submit", function(e){
-    e.preventDefault();
-    fetch(`${herokuUrl}/home`, {
-    //fetch('https://localhost:3000/home', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+const data = { entry };
+element.addEventListener("submit", function (e) {
+  e.preventDefault();
+  //fetch(`${herokuUrl}/home`, {
+  fetch("https://localhost:3000/home", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      // body.appendChild(data);
     })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        // body.appendChild(data);
-    })
-    .catch((error)=>{
-        console.log('Error: ', error);
-    })
-})
+    .catch((error) => {
+      console.log("Error: ", error);
+    });
+});
 
 getEntries();
